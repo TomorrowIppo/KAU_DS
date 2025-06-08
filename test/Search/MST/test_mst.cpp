4ad9690426d6a6613101a383aa2f4db103a4a1de
TomorrowIppo <bjk1624@gmail.com>
2025-06-09 02:10:44 +0900

    feat: Kruskal 알고리즘 및 전용 MST 전용 테스트 cpp 생성
    - GraphSearch namespace에 Kruskal 함수 구현
    - 이를 테스트 할 MST 전용 cpp 생성 및 검증 완료료

---
+++ test/Search/MST/test_mst.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "../../../include/Graph/AdjMatrix/AdjMatrix.hpp"
+#include "../../../include/Graph/AdjList/AdjList.hpp"
+#include "../../../include/Search/MST/Kruskal.hpp"
+
+using namespace ds;
+
+int main() {
+    AdjMatrix g(5);    // 무방향 그래프
+
+    // 단방향이 기본이므로 양방향 간선을 직접 넣어줌
+    g.insertEdge(1, 2, 1);
+    g.insertEdge(1, 3, 2);
+    g.insertEdge(2, 4, 11);
+    g.insertEdge(3, 5, 5);
+    g.insertEdge(5, 1, 7);
+
+    g.printGraph();
+    std::cout << "\n";
+    GraphSearch::kruskal(g);
+
+    return 0;
+}
